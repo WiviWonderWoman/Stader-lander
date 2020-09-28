f554b36d28b88f4264cb39d4f6f29b7373e9e16f
Wivianne Grapenholt <w.grapenholt@gmail.com>
2020-09-28 16:06:27 +0200

    hämtat land och stad

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+fetch('./land.json')
+.then(resp => resp.json())
+.then(data => console.log(data))
+
+fetch('./stad.json')
+.then(resp => resp.json())
+.then(data => console.log(data))
+
+
