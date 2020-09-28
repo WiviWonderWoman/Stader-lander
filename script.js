31662777e68f969163d8d094e78a82b6f6b94007
Wivianne Grapenholt <w.grapenholt@gmail.com>
2020-09-28 21:55:39 +0200

    fixat meny för länderna

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 'use strict';
+const app = 
+{
+    länder: [],
+    // value: "1234",
+
+}
+
 fetch('./land.json')
 .then(resp => resp.json())
-.then(data => console.log(data))
+.then(data => landMeny(data))
 
-fetch('./stad.json')
-.then(resp => resp.json())
-.then(data => console.log(data))
+function landMeny(data){
+     for (let index = 0; index < data.length; index++) {
+         const element = data[index];
+         app.länder.unshift(element);
+         document.getElementById('landMeny').innerHTML += `<li><button>`+element.countryname+`</button></li>`;
+        }
+        document.getElementById('landMeny').innerHTML += `<li><button id="visited">Besökt</button></li>`;
+}
+console.log(app.länder);
+
+// fetch('./stad.json')
+// .then(resp => resp.json())
+// .then(data => console.log(data))
 
 
