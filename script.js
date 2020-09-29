ccc9874a35dc15270e9cb8118d00bfcc04d518f0
Wivianne Grapenholt <w.grapenholt@gmail.com>
2020-09-29 17:09:08 +0200

    filtrering och visning

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 const app = 
 {
     länder: [],
-    städer: []
+    städer: [],
+    
 }
 
 fetch('./land.json')
@@ -28,21 +29,54 @@ function stad(data){
         const element = data[index];
         app.städer.push(element);
     }
-    let landMeny = document.getElementById('landMeny');
-    landMeny.addEventListener('click', function(event) {
-        console.log('event', event.target.id);
-    })
-    console.log(app.länder);
+    // let landMeny = document.getElementById('landMeny');
+    // landMeny.addEventListener('click', function(event) {
+    // console.log('event', event.target.id);
+    // let landID = event.target.id;
+
+    // let stadLand = app.städer.filter(a => a.countryid == landID); 
+    // console.log(stadLand);
+    // let stadMeny = stadLand.stadname;
+    // console.log(stadMeny);
+    // document.getElementById(landID).innerHTML += `<dd>`+stadMeny+`</dd>`;
+    // })
+    // console.log(app.länder);
 }
+    // let stadLand = app.städer.filter(a => a.countryid == event.target.id); 
+    //     console.log(stadLand);
+    // console.log(app.länder);
+
 
 function meny() {
+    let stadLand = [];
     for (let index = 0; index < app.länder.length; index++) {
         const land = app.länder[index];
         let landID = land.id;
         let landNamn = land.countryname;
-        console.log(land, landID, landNamn);
+        // console.log(land, landID, landNamn);
         document.getElementById('landMeny').innerHTML += `<dt id="`+landID+`">`+landNamn+`</dt>`;  
+        // console.log(stadMeny);
     }
+        console.log(app.länder);
+        console.log(stadLand);    
+    
+    let landMeny = document.getElementById('landMeny');
+        landMeny.addEventListener('click', function(event) {
+        console.log('event', event.target.id);
+        let landID = event.target.id;
+    
+        stadLand = app.städer.filter(a => a.countryid == landID); 
+        console.log(stadLand);
+        for (let index = 0; index < stadLand.length; index++) {
+            const stad = stadLand[index];
+            let stadID = stad.id;
+            let stadNamn = stad.stadname;
+            console.log(stadNamn);
+            document.getElementById(landID).innerHTML += `<dd>`+stadNamn+`</dd>`;
+        }
+        // let stadMeny = stadLand.stadname;
+    });
+    // console.log(stadLand);
     document.getElementById('landMeny').innerHTML += `<dt>Besökt</dt>`;
 }
   
