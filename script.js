9843e1836988615e7ac88bdd2eb48a3f5fcd488d
Wivianne Grapenholt <w.grapenholt@gmail.com>
2020-09-29 19:24:47 +0200

    stylat och till visning + kod

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ function land(data){
          app.länder.unshift(element);
         }
         meny();
-        console.log(app.städer);
 }
 
 
@@ -29,22 +28,8 @@ function stad(data){
         const element = data[index];
         app.städer.push(element);
     }
-    // let landMeny = document.getElementById('landMeny');
-    // landMeny.addEventListener('click', function(event) {
-    // console.log('event', event.target.id);
-    // let landID = event.target.id;
-
-    // let stadLand = app.städer.filter(a => a.countryid == landID); 
-    // console.log(stadLand);
-    // let stadMeny = stadLand.stadname;
-    // console.log(stadMeny);
-    // document.getElementById(landID).innerHTML += `<dd>`+stadMeny+`</dd>`;
-    // })
-    // console.log(app.länder);
 }
-    // let stadLand = app.städer.filter(a => a.countryid == event.target.id); 
-    //     console.log(stadLand);
-    // console.log(app.länder);
+
 
 
 function meny() {
@@ -54,30 +39,30 @@ function meny() {
         let landID = land.id;
         let landNamn = land.countryname;
         // console.log(land, landID, landNamn);
-        document.getElementById('landMeny').innerHTML += `<dt id="`+landID+`">`+landNamn+`</dt>`;  
+        document.getElementById('landMeny').innerHTML += `<li ><button id="`+landID+`">`+landNamn+`</button></li>`;  
         // console.log(stadMeny);
-    }
-        console.log(app.länder);
-        console.log(stadLand);    
+    }   
     
     let landMeny = document.getElementById('landMeny');
         landMeny.addEventListener('click', function(event) {
-        console.log('event', event.target.id);
+            document.getElementById('content').innerHTML ="";
+            console.log(event.target.id);
         let landID = event.target.id;
+       
     
         stadLand = app.städer.filter(a => a.countryid == landID); 
-        console.log(stadLand);
+        
         for (let index = 0; index < stadLand.length; index++) {
             const stad = stadLand[index];
             let stadID = stad.id;
             let stadNamn = stad.stadname;
             console.log(stadNamn);
-            document.getElementById(landID).innerHTML += `<dd>`+stadNamn+`</dd>`;
+            document.getElementById('content').innerHTML += `<li><button id="`+stadID+`">`+stadNamn+`</button></li>`;
         }
         // let stadMeny = stadLand.stadname;
     });
     // console.log(stadLand);
-    document.getElementById('landMeny').innerHTML += `<dt>Besökt</dt>`;
+    document.getElementById('landMeny').innerHTML += `<li><button id="visited">Besökt</button></li>`;
 }
   
 
