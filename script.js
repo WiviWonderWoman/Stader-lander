778b6ef4c30d7898389f87ce925dd067462e70a7
Wivianne Grapenholt <w.grapenholt@gmail.com>
2020-09-29 15:37:36 +0200

    added event listener

--- script.js
+++ script.js
@@ -2,9 +2,7 @@
 const app = 
 {
     länder: [],
-    städer: [],
-    landID: "",
-    stadID: "",
+    städer: []
 }
 
 fetch('./land.json')
@@ -15,12 +13,11 @@ function land(data){
      for (let index = 0; index < data.length; index++) {
          const element = data[index];
          app.länder.unshift(element);
-        
         }
-        meny(); 
+        meny();
         console.log(app.städer);
 }
- 
+
 
 fetch('./stad.json')
 .then(resp => resp.json())
@@ -31,16 +28,20 @@ function stad(data){
         const element = data[index];
         app.städer.push(element);
     }
+    let landMeny = document.getElementById('landMeny');
+    landMeny.addEventListener('click', function(event) {
+        console.log('event', event.target.id);
+    })
     console.log(app.länder);
 }
 
 function meny() {
     for (let index = 0; index < app.länder.length; index++) {
         const land = app.länder[index];
-        landID = land.id;
+        let landID = land.id;
         let landNamn = land.countryname;
         console.log(land, landID, landNamn);
-        document.getElementById('landMeny').innerHTML += `<dt>`+landNamn+`</dt>`;  
+        document.getElementById('landMeny').innerHTML += `<dt id="`+landID+`">`+landNamn+`</dt>`;  
     }
     document.getElementById('landMeny').innerHTML += `<dt>Besökt</dt>`;
 }
